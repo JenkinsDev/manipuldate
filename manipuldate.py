2dfda9b25df43a150f1d46514c43b2058b6d3a0d
David Jenkins <david.nicholas.jenkins@gmail.com>
2014-11-17 15:02:41 -0500

    Added sub month(s) methods.

--- manipuldate.py
+++ manipuldate.py
@@ -6,6 +6,23 @@ class Manipuldate(datetime.datetime):
 
     def _new_instance(self, year=0, month=0, day=0, hour=0, minute=0,
                       second=0, microsecond=0, tzinfo=None):
+        """ Creates a new instance of Manipuldate based on the supplied information,
+        if something is set to 0, None or False then we will use the corresponding
+        attributes value.
+
+        Parameters:
+            :param year: Numerical value that represents years
+            :param month: Numerical value that represents months [1-12]
+            :param day: Numerical value that represents days [1-(28-31)]
+            :param hour: Numerical value that represents hours [0-24]
+            :param minute: Numerical value that represents minutes [0-60]
+            :param second: Numerical value that represents seconds [0-60]
+            :param second: Numerical value that represents microseconds [0-1000000]
+            :param tzinfo: TimeZone information
+
+        Returns:
+            Manipuldate Instance
+        """
         year = year or self.year
         month = month or self.month
         day = day or self.day
@@ -17,10 +34,30 @@ class Manipuldate(datetime.datetime):
         return Manipuldate(year, month, day, hour, minute, second, microsecond, tzinfo)
 
     def sub_months(self, months):
+        """ Subtracts the amount of provided months to our datetime object
+        then returns a new instance of Manipuldate because of how datetime
+        works within python, no editable properties.
+
+        Parameters:
+            :param months: The number of months to sub from the current date.
+
+        Returns:
+            Manipuldate Instance
+        """
         month, year = self.month - months, self.year
         while month <= 0:
             year -= 1
             month += 12
+        return self._new_instance(year=year, month=month)
+
+    def sub_month(self):
+        """ Convenience method for subtracting 1 month, shorter than using
+        sub_months, anmd more pythonic.
+
+        Returns:
+            Manipuldate Instance
+        """
+        return self.sub_months(1)
 
     def add_months(self, months):
         """ Adds the amount of provided months to our datetime object
@@ -40,8 +77,8 @@ class Manipuldate(datetime.datetime):
         return self._new_instance(year=year, month=month)
 
     def add_month(self):
-        """ Convenience method for add 1 month, shorter than
-        using add_months, and more pythonic.
+        """ Convenience method for adding 1 month, shorter than using
+        add_months, and more pythonic.
 
         Returns:
             Manipuldate Instance
--- tests.py
+++ tests.py
@@ -10,6 +10,10 @@ class TestBasicDateArithmetic(unittest.TestCase):
         # date so we can solidly check the arithmetic
         self.manip = Manipuldate.strptime("11/21/06", "%m/%d/%y")
 
+    def test_response_is_instance_of_manipuldate_addition(self):
+        one_month = self.manip.add_month()
+        self.assertTrue(isinstance(one_month, Manipuldate))
+
     def test_add_one_month(self):
         one_month = self.manip.add_month()
         self.assertEqual(one_month.month, 12)
@@ -20,9 +24,26 @@ class TestBasicDateArithmetic(unittest.TestCase):
         self.assertEqual(two_months.year, 2007)
 
     def test_add_one_hundred_months_and_year_wrap(self):
+        # We want to make sure that when adding (n) months that the
+        # year will always continuing wrapping until we have reached
+        # the correct date.
         one_hundred_months = self.manip.add_months(100)
         self.assertEqual(one_hundred_months.month, 3)
         self.assertEqual(one_hundred_months.year, 2015)
+
+    def test_sub_on_month(self):
+        one_month = self.manip.sub_month()
+        self.assertEqual(one_month.month, 10)
+
+    def test_sub_eleven_months_and_year_wraps(self):
+        eleven_months = self.manip.sub_months(11)
+        self.assertEqual(eleven_months.month, 12)
+        self.assertEqual(eleven_months.year, 2005)
+
+    def test_sub_one_hundred_months_and_year_wraps(self):
+        one_hundred_months = self.manip.sub_months(100)
+        self.assertEqual(one_hundred_months.month, 7)
+        self.assertEqual(one_hundred_months.year, 1998)
 
 
 if __name__ == "__main__":
